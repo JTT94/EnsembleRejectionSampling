a6611206cf261b0d61f6e921e1d6a1418d10687d
JTT94 <jtthornton1994@gmail.com>
2020-08-03 18:36:45 +0100

    experiment run

--- experiments/run.py
+++ experiments/run.py
@@ -3,13 +3,25 @@ import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 print(dir_path)
+
+mlflow.projects.run(
+    os.path.join(dir_path,'./run_model'),
+    backend='local',
+    synchronous=False,
+    parameters = {'T' : 50,
+                 'dimension' : 1,
+                 'n_samples': 2,
+                 'n_particles' : 2000,
+                 'n_workers' : 1})
+
+
 mlflow.projects.run(
     os.path.join(dir_path,'./run_model'),
     backend='local',
     synchronous=True,
-    parameters = {'T' : 50,
+    parameters = {'T' : 200,
                  'dimension' : 1,
-                 'n_samples': 2,
+                 'n_samples': 1,
                  'n_particles' : 2000,
                  'n_workers' : 1})
 
