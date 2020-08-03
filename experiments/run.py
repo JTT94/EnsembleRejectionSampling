cc75735651a36e7afb588af8380f04e359e28e80
JTT94 <jtthornton1994@gmail.com>
2020-08-03 19:22:41 +0100

    wait for async runs to finish

--- experiments/run.py
+++ experiments/run.py
@@ -1,28 +1,39 @@
 import mlflow
-import os 
+import os, sys
+import time
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 print(dir_path)
 
-mlflow.projects.run(
-    os.path.join(dir_path,'./run_model'),
-    backend='local',
-    synchronous=True,
-    parameters = {'T' : 50,
-                 'dimension' : 1,
-                 'n_samples': 2,
-                 'n_particles' : 2000,
-                 'n_workers' : 1})
+run_ids = []
 
-
-mlflow.projects.run(
+run1 = mlflow.projects.run(
     os.path.join(dir_path,'./run_model'),
     backend='local',
     synchronous=False,
-    parameters = {'T' : 200,
+    parameters = {'T' : 50,
+                 'dimension' : 1,
+                 'n_samples': 2,
+                 'n_particles' : 500,
+                 'n_workers' : 1})
+run_ids.append(run1.run_id)
+
+run2 = mlflow.projects.run(
+    os.path.join(dir_path,'./run_model'),
+    backend='local',
+    synchronous=False,
+    parameters = {'T' : 100,
                  'dimension' : 1,
                  'n_samples': 1,
-                 'n_particles' : 2000,
+                 'n_particles' : 500,
                  'n_workers' : 1})
+obj = mlflow.tracking.MlflowClient()
 
+run_ids.append(run2.run_id)
+
+def runs_finished(run_ids):
+    return all(obj.get_run(run_id).info.status == 'FINISHED' for run_id in run_ids)
+
+while not runs_finished(run_ids):
+    time.sleep(1.)
 
