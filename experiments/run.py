35bfc70471c928abc43fc0cac46ae6be2b678380
JTT94 <jtthornton1994@gmail.com>
2020-08-06 17:13:32 +0100

    update param

--- experiments/run.py
+++ experiments/run.py
@@ -3,60 +3,36 @@ import os, sys
 import time
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-print(dir_path)
-
 run_ids = []
-
-
-
 param_list = []
+synchronous=True
 
-param_list.append({'model_tag':'hardobstacle',
-                 'T' : 250,
-                 'dimension' : 1,
-                 'n_samples': 500,
-                 'n_particles' : 250,
-                 'n_workers' : 1})
-param_list.append({'model_tag':'hardobstacle',
-                 'T' : 250,
-                 'dimension' : 1,
-                 'n_samples': 500,
-                 'n_particles' : 500,
-                 'n_workers' : 1})
-param_list.append({'model_tag':'hardobstacle',
-                 'T' : 250,
-                 'dimension' : 1,
-                 'n_samples': 500,
-                 'n_particles' : 1250,
-                 'n_workers' : 1})
+T=100
+n_samples=500
+n_workers=1
 
-param_list.append({'model_tag':'nonLinearAR',
-                 'T' : 500,
-                 'dimension' : 1,
-                 'n_samples': 500,
-                 'n_particles' : 500,
-                 'n_workers' : 1})
-param_list.append({'model_tag':'nonLinearAR',
-                 'T' : 500,
-                 'dimension' : 1,
-                 'n_samples': 500,
-                 'n_particles' : 1000,
-                 'n_workers' : 1})
-param_list.append({'model_tag':'nonLinearAR',
-                 'T' : 500,
-                 'dimension' : 1,
-                 'n_samples': 500,
-                 'n_particles' : 2000,
-                 'n_workers' : 1})    
 
+for dimension in [1]:
+    for T in [100, 250, 500]:
+        for num_particles in [T, 2*T, 5*T]:
+    
+            params = {'model_tag':'nonLinearAR',
+                    'T' : T,
+                    'dimension' : dimension,
+                    'n_samples': n_samples//n_workers ,
+                    'n_particles' : num_particles,
+                    'n_workers' : n_workers}
+            param_list.append(params)
+    
 for id, params in enumerate(param_list):
-    if params['T'] == 500:
-        run = mlflow.projects.run(
-            os.path.join(dir_path,'./run_model'),
-            backend='local',
-            synchronous=False,
-            parameters = params)
-        run_ids.append(run.run_id)
+    
+    run = mlflow.projects.run(
+        os.path.join(dir_path,'./run_model'),
+        backend='local',
+        synchronous=synchronous,
+        parameters = params)
+
+    run_ids.append(run.run_id)
 
 obj = mlflow.tracking.MlflowClient()
 
@@ -64,8 +40,5 @@ def runs_finished(run_ids):
     return all(obj.get_run(run_id).info.status == 'FINISHED' for run_id in run_ids)
 
 while not runs_finished(run_ids):
-    time.sleep(1.)
+    time.sleep(.1)
 
-#obj.get_run(run1.run_id)
-
-#obj.get_metric_history(run1.run_id, 'w0-iter')
